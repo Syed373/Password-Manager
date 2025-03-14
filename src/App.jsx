e6f95c7d87ce2c11a21510176cd8fcb24e033029
Syed373 <sua030703@gmail.com>
2025-03-14 22:01:00 +0530

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Header/Navbar'
+import AddNew from './components/Body/AddNew'
 import './App.css'
 
 function App() {
@@ -7,9 +8,9 @@ function App() {
 
   return (
     <>
-      <div className="absolute inset-0 -z-10 size-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:1rem_2rem]">
-      <div className="absolute inset-0 bg-[radial-gradient(circle_400px_at_50%_250px,#C9EBFF,transparent)]" />
+      <div>
         <Navbar></Navbar>
+        <AddNew></AddNew>
       </div>
     </>
   )
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -3,22 +3,17 @@ import React from "react";
 export default function Navbar() {
     return (
         <>
-            <nav className=" h-28 w-screen flex justify-around items-center gap-96">
-                <div>
-                    <div className="text-cyan-500 text-[40px] font-extrabold">
+            <nav className=" h-28 w-screen flex justify-around items-center">
+                    <div className="text-cyan-500 text-3xl font-extrabold">
                         <span className="">&lt;Pass</span>
                         <span className="text-black">OP/&gt;</span>
                     </div>
-                    <div className="text-sm text-center">
-                        <p> Your own Password Manager</p>
-                    </div>
-                </div>
                 <div className="flex justify-center items-center gap-8">
                     <a href="https://github.com/Syed373/Password-Manager" target="_blank">
                         <lord-icon
                             src="https://cdn.lordicon.com/ioihllwu.json"
                             trigger="hover"
-                            className="w-15 h-15 pt-2">
+                            className="w-13 h-13 pt-2">
                         </lord-icon></a>
                     <a href="/" className="underline text-2xl ">Login</a>
                 </div>
